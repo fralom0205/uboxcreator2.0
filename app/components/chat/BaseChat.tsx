d1d52598d32efde99252c53979e0ab2a3a17df61
fralom0205 <francesco.lomuzio@universitybox.com>
2025-01-14 15:23:25 +0100

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -519,7 +519,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                         minHeight: TEXTAREA_MIN_HEIGHT,
                         maxHeight: TEXTAREA_MAX_HEIGHT,
                       }}
-                      placeholder="How can Bolt help you today?"
+                      placeholder="Cosa vuoi creare con UBox Creator?"
                       translate="no"
                     />
                     <ClientOnly>
